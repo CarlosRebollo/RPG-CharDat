831f985c892e0782ccba9f6243b418bd386eb09e
CarlosRebollo <carlos.alrai@gmail.com>
2022-04-11 18:37:35 +0200

    1

--- app/src/main/java/ies/quevedo/chardat/ui/personaje/AddPersonajeFragment1.kt
+++ app/src/main/java/ies/quevedo/chardat/ui/personaje/AddPersonajeFragment1.kt
@@ -1,18 +1,18 @@
 package ies.quevedo.chardat.ui.personaje
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import ies.quevedo.chardat.R
 
-class AddPersonajeFragment : Fragment() {
+class AddPersonajeFragment1 : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_add_personaje, container, false)
+        return inflater.inflate(R.layout.fragment_add_personaje1, container, false)
     }
 }
